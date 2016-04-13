dfeca722084cca9a5c8ad85bf1d2a84b2641089d
Jake2000 <razumov.evgeny@yandex.ru>
2016-04-13 11:43:28 +0300

    update pagination

--- addon/components/ds-table-thead/component.js
+++ addon/components/ds-table-thead/component.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import layout from './template';
-const { Component, A, computed } = Ember;
+const { Component } = Ember;
 export default Component.extend({
     layout,
     tagName: 'thead',
