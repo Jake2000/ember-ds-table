b4917fc022b43d00807c4e00d46d77d6401cdc9a
Jake2000 <razumov.evgeny@yandex.ru>
2016-04-15 11:19:38 +0300

    replace pagination

--- addon/components/ds-table-pagination/component.js
+++ addon/components/ds-table-pagination/component.js
@@ -3,68 +3,17 @@ import layout from './template';
 const { Component, computed, observer } = Ember;
 export default Component.extend({
     layout,
-    count: 0,
-    currentPageNumber: 1,
+    countRecords: 0,
     skip: 0,
     limit: 10,
-    gotoBackEnabled: computed.gt('currentPageNumber', 1),
-    gotoForwardEnabled: computed('skip', 'count', function () {
-        return this.get('currentPageNumber') * this.get('limit') < this.get('count');
+    current: 1,
+    count: computed('countRecords', 'limit', function() {
+        let { limit, countRecords } = this.getProperties('limit', 'countRecords');
+        const count = countRecords / limit;
+        return (0 === count % 1) ? count : (Math.floor(count) + 1);
     }),
-    pagesCount: computed('count', 'limit', function() {
-        const pagesCount = this.get('count') / this.get('limit');
-        return (0 === pagesCount % 1) ? pagesCount : (Math.floor(pagesCount) + 1);
-    }),
-    _setupPaginationQueryParams: observer('limit', 'currentPageNumber', function() {
-        const {
-            limit,
-            currentPageNumber
-        } = this.getProperties('limit', 'currentPageNumber');
-
-        const skip = limit * (currentPageNumber - 1);
-        this.setProperties({
-          skip: skip,
-          limit: limit
-        });
-    }),
-    actions: {
-        gotoFirstPage() {
-            if (!this.get('gotoBackEnabled')) {
-               return;
-            }
-
-            this.set('currentPageNumber', 1);
-        },
-
-        gotoPrevPage() {
-            if (!this.get('gotoBackEnabled')) {
-               return;
-            }
-
-            if (this.get('currentPageNumber') > 1) {
-                this.decrementProperty('currentPageNumber');
-            }
-        },
-        gotoNextPage() {
-            if (!this.get('gotoForwardEnabled')) {
-               return;
-            }
-
-            let {
-                currentPageNumber,
-                limit,
-                count
-            } = this.getProperties('currentPageNumber', 'limit', 'count');
-            if (count > limit * (currentPageNumber - 1)) {
-                this.incrementProperty('currentPageNumber');
-            }
-        },
-        gotoLastPage() {
-            if (!this.get('gotoForwardEnabled')) {
-               return;
-            }
-
-            this.set('currentPageNumber', this.get('pagesCount'));
-        }
-    }
+    _updateLimit: observer('current', function() {
+        let { limit, current } = this.getProperties('limit', 'current');
+        this.set('skip', limit * (current - 1));
+    })
 });
--- addon/components/ds-table/component.js
+++ addon/components/ds-table/component.js
@@ -13,40 +13,45 @@ const {
     isNone
 } = Ember;
 
+const O = Ember.Object;
+
 export default Component.extend({
     layout,
     tagName: 'table',
     store: service(),
     modelName: '',
-    columns: A([]),
-    filter: {
-        skip: 0,
-        limit: 10
+    columns: [],
+    skip: 0,
+    limit: 10,
+    meta: {
+        count: 0
     },
-    meta: {},
     reload: false,
     didReceiveAttrs() {
+        //columns
         let columns = A(this.get('columns')), ret = A([]);
         columns.forEach(item => {
-            item = Ember.Object.create(item);
-            let isVisible = item.get('isVisible');
-            if(isNone(isVisible)) {
+            item = O.create(item);
+            if(isNone(item.get('isVisible'))) {
                 item.set('isVisible', true);
             }
             ret.addObject(item);
         });
         columns.replace(0, columns.length, ret);
+        //meta
+        this.set('meta', O.create(this.get('meta')));
     },
-    model: computed('reload', 'filter.{skip,limit}', function() {
+    model: computed('reload', 'skip', 'limit', function() {
         const {
             store,
             modelName,
-            filter
-        } = this.getProperties('store', 'modelName', 'filter');
+            skip,
+            limit
+        } = this.getProperties('store', 'modelName', 'skip', 'limit');
         return DS.PromiseArray.create({
-            promise: store.query(modelName, { filter: filter })
+            promise: store.query(modelName, { filter: { skip: skip, limit: limit } })
                 .then(result => {
-                    this.set('meta', result.get('meta'));
+                    this.set('meta.count', result.get('meta.count'));
                     return result;
                 })
         });
