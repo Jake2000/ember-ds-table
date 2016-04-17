8cc0103c1bcfcd502dc346b683b74c918084bec1
Evgeny Razumov <razumov.evgeni@yandex.ru>
2016-04-17 17:40:17 +0300

    update url prefix

--- tests/dummy/app/mirage/config.js
+++ tests/dummy/app/mirage/config.js
@@ -7,7 +7,7 @@ export default function() {
 
     Note: these only affect routes defined *after* them!
   */
-  //this.urlPrefix = 'http://localhost:8000';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  this.urlPrefix = 'http://jake2000.github.io/ember-ds-table/';    // make this `http://localhost:8080`, for example, if your API is on a different server
   this.namespace = 'api';    // make this `api`, for example, if your API is namespaced
   this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
